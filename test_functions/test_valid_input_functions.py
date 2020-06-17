a6c7107ec87ab94bc99b7179064eab3391adf2da
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-17 16:54:04 -0500

    5th test working

--- test_functions/test_valid_input_functions.py
+++ test_functions/test_valid_input_functions.py
@@ -19,10 +19,10 @@ class MyTestCase(unittest.TestCase):
         test_results = 'Score is above maximum. Please enter score between 0 and 100'
         self.assertEqual(test_results, valid.score_input('test', 101))
 
-    # def test_score_input_non_numeric(self):
-    #     test_results = 'Invalid test score, Please try again.'
-    #     self.assertEqual(test_results, valid.score_input('test', 'test'))
-    #
+    def test_score_input_non_numeric(self):
+        test_results = 'Invalid test score, Please try again'
+        self.assertEqual(test_results, valid.score_input('test', 'test'))
+
     # def test_score_input_invalid_message(self):
     #     test_results = 'test message'
     #     self.assertEqual(test_results, valid.score_input('test', 'test', 'test message'))
