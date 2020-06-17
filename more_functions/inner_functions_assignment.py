135aea132aba14acc42e671330dec7d22620da65
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-17 17:41:18 -0500

    Initial tests completed

---
+++ more_functions/inner_functions_assignment.py
@@ -0,0 +1,28 @@
+"""
+Program: inner_functions_assignment.py
+Author: Paul Elsea
+Last Modified: 06/17/2020
+
+Program to demonstrate inner functions
+"""
+
+'''This takes in an input string, integer, and a error message. There are default
+values for the integer and error message.
+:name: string test name
+:score: integer score of test
+:error_message: string that is printed if invalid input is received.
+:returns: Test name and score as a string'''
+def measurements(inputA, inputB):
+    perim_result = 0.0
+    area_result = 0.0
+
+    def area(a_list):
+        return
+        pass
+    def perimeter(a_list):
+        return
+        pass
+
+
+
+    print('Perimeter = ' + str(perim_result) + '   Area = ' + str(area_result))
---
+++ test_functions/test_inner_functions.py
@@ -0,0 +1,23 @@
+"""
+Program: test_valid_input_functions.py
+Author: Paul Elsea
+Last Modified: 06/17/2020
+
+Program to test score_input function
+"""
+
+import unittest
+from more_functions import inner_functions_assignment as inner
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_measurements_rectangle(self):
+        self.assertEqual(inner.measurements([2.1, 3.4]), 'Perimeter = 11.0    Area = 7.14')
+
+    def test_measurements_square(self):
+        self.assertEqual(inner.measurements([3.5]), 'Perimeter = 14.0    Area = 12.25')
+
+
+if __name__ == '__main__':
+    unittest.main()
