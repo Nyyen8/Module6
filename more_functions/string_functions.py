4c325dce2f5f72eaac2daa8a4b0c716da02e8d36
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-17 14:38:01 -0500

    Finalized test_string_functions.py

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -5,8 +5,22 @@ Last Modified: 06/17/2020
 
 Program to concatenate a string based off a chosen number of repetitions
 """
-def multiply_string():
-    pass
+'''String to be repeated and Int for how many repetitions'''
+class_string = 'Cuttlefish psychology '
+repetitions = 3
+
+'''This takes in an input string and a number of repetitions, 
+concatenates the string for how ever many repetitions are input, 
+then returns the resulting string
+:string_input: string to be multiplied
+:repetitions_input: number of times to multiply the string
+:returns: returns multiplied input string'''
+def multiply_string(string_input, repetitions_input):
+    multiplied_string = ''
+    while repetitions_input > 0:
+        multiplied_string = multiplied_string + string_input
+        repetitions_input -= 1
+    return multiplied_string
 
 if __name__ == '__main__':
     try:
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -1,12 +1,17 @@
 import unittest
 from more_functions import string_functions as strings
 
-
+'''Test that calls multiply_string method with test string and integer
+test_string: a string to be repeated
+test_repetitions: how many times to repeat the test_string
+return: true/false assertion of whether return of function and expected result are the same'''
 class MyTestCase(unittest.TestCase):
     def test_multiple_string(self):
-        self.assertEqual('PaulPaulPaul', strings.multiply_string())
+        test_string = 'Paul'
+        test_repetitions = 3
+        self.assertEqual('PaulPaulPaul', strings.multiply_string(test_string, test_repetitions))
 
 
-
+'''Default main method call to invoke test'''
 if __name__ == '__main__':
     unittest.main()
