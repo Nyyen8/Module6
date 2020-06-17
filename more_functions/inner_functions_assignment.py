939de3331fe977454277f5698d35218499b8ae62
Paul Elsea <Paul.M.Elsea@gmail.com>
2020-06-17 18:36:16 -0500

    2nd test working

--- more_functions/inner_functions_assignment.py
+++ more_functions/inner_functions_assignment.py
@@ -18,14 +18,20 @@ def measurements(a_list):
 
     def perimeter(a_list):
         perimeter_result = 0
-        for side in a_list:
-            perimeter_result += side * 2
+        if len(a_list) == 1:
+            perimeter_result = a_list[0] * 4
+        else:
+            for side in a_list:
+                perimeter_result += side * 2
         return perimeter_result
 
     def area(a_list):
-        sideA = a_list[0]
-        sideB = a_list[1]
-        area_result = sideA * sideB
+        if len(a_list) == 1:
+            area_result = a_list[0] * a_list[0]
+        elif len(a_list) == 2:
+            sideA = a_list[0]
+            sideB = a_list[1]
+            area_result = sideA * sideB
         return area_result
 
     return('Perimeter = ' + str(perimeter(a_list)) + '  Area = ' + str(area(a_list)))
